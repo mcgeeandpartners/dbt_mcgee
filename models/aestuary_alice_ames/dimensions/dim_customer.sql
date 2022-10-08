f4dbafa39e001fc663b05a9c9c8b1b3fae04637d
caroline@aestuary.com <caroline@aestuary.com>
2022-10-08 16:49:53 +0000

    dim_customer changes

--- models/aestuary_alice_ames/dimensions/dim_customer.sql
+++ models/aestuary_alice_ames/dimensions/dim_customer.sql
@@ -1,20 +1,27 @@
-select distinct c.ID as CUSTOMER_ID,
-c.FIRST_NAME as FIRST_NAME,
-c.CREATED_AT as CREATED_AT,
-c.EMAIL as EMAIL,
-c.LAST_NAME as LAST_NAME,
-c.STATE as IS_ACTIVE,
-c.TAX_EXEMPT as IS_TAX_EXEMPT,
-c.UPDATED_AT as UPDATED_AT,
-ca.PHONE as PHONE,
-ca.ADDRESS_1 as ADDRESS_1,
-ca.ADDRESS_2 as ADDRESS_2,
-ca.CITY as CITY,
-ca.COUNTRY as COUNTRY,
-ca.PROVINCE as STATE,
-ca.ZIP as ZIPCODE,
-ca.LATITUDE as LATITUDE,
-ca.LONGITUDE as LONGITUDE,
+with customer_address_cte as (
+
+  select *, ROW_NUMBER () OVER (PARTITION BY ca.CUSTOMER_ID ORDER BY ca.ID desc) AS TO_SELECT  from "AESTUARY_RAW"."SHOPIFY"."CUSTOMER_ADDRESS" ca order by ID desc
+
+)
+select
+{{ dbt_utils.surrogate_key(['c.ID']) }} as CUSTOMER_KEY,
+c.ID as CUSTOMER_ID,
+c.FIRST_NAME as CUSTOMER_FIRST_NAME,
+c.CREATED_AT as CUSTOMER_CREATED_AT,
+c.EMAIL as CUSTOMER_EMAIL,
+c.LAST_NAME as CUSTOMER_LAST_NAME,
+lower(c.STATE) as CUSTOMER_ACTIVITY_STATE,
+c.TAX_EXEMPT as CUSTOMER_IS_TAX_EXEMPT,
+c.UPDATED_AT as CUSTOMER_UPDATED_AT,
+case when ca.PHONE = '' then NULL else ca.PHONE end as CUSTOMER_PHONE,
+case when ca.ADDRESS_1 = '' then NULL else ca.ADDRESS_1 end as CUSTOMER_ADDRESS_1,
+case when ca.ADDRESS_2 = '' then NULL else ca.ADDRESS_2 end as CUSTOMER_ADDRESS_2,
+case when ca.CITY = '' then NULL else ca.CITY end as CUSTOMER_CITY,
+case when ca.COUNTRY = '' then NULL else ca.COUNTRY end as CUSTOMER_COUNTRY,
+case when ca.PROVINCE = '' then NULL else ca.PROVINCE end as CUSTOMER_STATE,
+case when ca.ZIP = '' then NULL else ca.ZIP end as CUSTOMER_ZIPCODE,
+case when ca.LATITUDE = '' then NULL else ca.LATITUDE end as CUSTOMER_LATITUDE,
+case when ca.LONGITUDE = '' then NULL else ca.LONGITUDE end as CUSTOMER_LONGITUDE,
 'SHOPIFY' as CHANNEL_SOURCE
 from
-"AESTUARY_RAW"."SHOPIFY"."CUSTOMER" c inner join "AESTUARY_RAW"."SHOPIFY"."CUSTOMER_ADDRESS" ca on c.ID = ca.CUSTOMER_ID
+"AESTUARY_RAW"."SHOPIFY"."CUSTOMER" c left join customer_address_cte ca on c.ID = ca.CUSTOMER_ID and ca.TO_SELECT = 1
