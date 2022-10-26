823a1be1f9481b717966682f7030f48f93e4fa29
caroline@aestuary.com <caroline@aestuary.com>
2022-10-26 16:49:51 +0000

    changes in facts, dim & stg models

--- models/aestuary_alice_ames/facts/fact_order_line_item.sql
+++ models/aestuary_alice_ames/facts/fact_order_line_item.sql
@@ -20,17 +20,20 @@
 -- )
 
 select
-ol.ORDER_LINE_ITEM_KEY, 
+oli.ORDER_LINE_ITEM_KEY, 
 c.CUSTOMER_KEY,
 d.DATE_KEY,
 p.PRODUCT_VARIANT_KEY,
 disc.DISCOUNT_KEY,
-ol.ORDER_ID,
-ol.ORDER_LINE_ITEM_ID,
-DATE(ol.ORDER_CREATED_TS) AS ORDER_CREATED_DATE,
-ol.ORDER_LINE_ITEM_PRICE as ORDER_LINE_ITEM_PRICE
-from {{  ref('dim_order_line_item') }} ol
-left join {{ ref('dim_customer') }} c on ol.CUSTOMER_ID = c.CUSTOMER_ID
-left join {{ ref('dim_date') }} d on d."DATE" = DATE(ol.ORDER_CREATED_TS)
-left join {{ ref('dim_product_variant') }} p on ol.PRODUCT_ID = p.PRODUCT_ID and ol.PRODUCT_VARIANT_ID = p.PRODUCT_VARIANT_ID
-left join {{ ref('dim_discount') }}  disc on COALESCE(ol.ORDER_DISCOUNT_CODE,'NO_DISCOUNT') = disc.DISCOUNT_CODE
+oli.ORDER_ID,
+oli.ORDER_LINE_ITEM_ID,
+oli.CURRENCY,
+DATE(stg.ORDER_CREATED_TS) AS ORDER_CREATED_DATE,
+oli.LINE_ITEM_WEIGHT,
+stg.ORDER_LINE_ITEM_PRICE as ORDER_LINE_ITEM_PRICE
+from {{  ref('stg_holding_table') }} stg
+join {{ ref ('dim_order_line_item')}} oli on stg.ORDER_LINE_ITEM_ID = oli.ORDER_LINE_ITEM_ID
+left join {{ ref('dim_customer') }} c on stg.CUSTOMER_ID = c.CUSTOMER_ID
+left join {{ ref('dim_date') }} d on d."DATE" = DATE(stg.ORDER_CREATED_TS)
+left join {{ ref('dim_product_variant') }} p on stg.PRODUCT_ID = p.PRODUCT_ID and stg.PRODUCT_VARIANT_ID = p.PRODUCT_VARIANT_ID
+left join {{ ref('dim_discount') }}  disc on COALESCE(stg.ORDER_DISCOUNT_CODE,'NO_DISCOUNT') = disc.DISCOUNT_CODE
