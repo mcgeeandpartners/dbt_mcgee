2eb773c0eb53b5755b42c07c582e92280547a94f
caroline@aestuary.com <caroline@aestuary.com>
2022-11-16 00:36:08 +0000

    Removed collection from fact table

--- models/aestuary_alice_ames/facts/fact_order_line_item.sql
+++ models/aestuary_alice_ames/facts/fact_order_line_item.sql
@@ -3,7 +3,6 @@ oli.ORDER_LINE_ITEM_KEY,
 c.CUSTOMER_KEY,
 d.DATE_KEY,
 p.PRODUCT_VARIANT_KEY,
-cp.COLLECTION_KEY,
 oli.ORDER_ID,
 oli.ORDER_LINE_ITEM_ID,
 oli.CURRENCY,
@@ -15,4 +14,3 @@ join {{ ref ('dim_order_line_item')}} oli on stg.ORDER_LINE_ITEM_ID = oli.ORDER_
 left join {{ ref('dim_customer') }} c on stg.CUSTOMER_ID = c.CUSTOMER_ID
 left join {{ ref('dim_date') }} d on d."DATE" = DATE(stg.ORDER_CREATED_TS)
 left join {{ ref('dim_product_variant') }} p on stg.PRODUCT_VARIANT_ID = p.PRODUCT_VARIANT_ID
-left join {{ ref('dim_collection_shopify')}} cp on stg.COLLECTION_ID = cp.COLLECTION_ID
