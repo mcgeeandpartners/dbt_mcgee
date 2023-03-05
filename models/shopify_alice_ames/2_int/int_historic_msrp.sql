0d8324067e2634f2868c50ce9d81222a5ad68ec9
Ovais Siddiqui <muhd.ovaissiddiqui@gmail.com>
2023-03-05 16:34:53 -0600

    materialization changes

--- models/shopify_alice_ames/2_int/int_historic_msrp.sql
+++ models/shopify_alice_ames/2_int/int_historic_msrp.sql
@@ -2,6 +2,12 @@
     When the founders run a sale, they will update the product prices instead of using the product discount code - which will make the order appear as if it never received a discount even though the product was inherently discounted.
     For data prior to the product_variant snapshot table, Caroline has given a logic to get historic MSRP. In simple terms, its the max trailing price of a product. A decline in the product price would mean that its price was updated by founders.
  #}
+{{
+  config(
+    materialized = 'table'
+    )
+}}
+
 with cte_max_daily_price as (
 
     select 
--- models/shopify_alice_ames/3_transform/transform_order_line_item_alice_ames.sql
+++ models/shopify_alice_ames/3_transform/transform_order_line_item_alice_ames.sql
@@ -1,3 +1,8 @@
+{{
+  config(
+    materialized = 'table'
+    )
+}}
 select 
     oli.order_line_item_id,
     oli.order_id,
