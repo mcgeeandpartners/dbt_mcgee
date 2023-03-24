f9b5a1e7d945e483b9e518904397455df4cdb774
Ovais Siddiqui <muhd.ovaissiddiqui@gmail.com>
2023-03-23 23:55:31 -0500

    remove msrp fields from Fact for AD

--- models/shopify_american_duchess/5_fact/fact_order_line_item_ad.sql
+++ models/shopify_american_duchess/5_fact/fact_order_line_item_ad.sql
@@ -17,13 +17,15 @@ select
     sum(order_line_item_units_product) over (partition by o.order_id) as order_units_product,
     sum(order_line_item_units_route) over (partition by o.order_id) as order_units_route,
     {# max(iff(is_vendor_route = false, i.msrp, 0)) over (partition by o.order_id) * order_units_product as order_gross_revenue_product, --msrp is on the product level so we take the max #}
-    sum(iff(is_vendor_route = false, i.msrp, 0) * oli.order_line_item_units) over (partition by o.order_id) as order_gross_revenue_product,
+    --For American Duchess, we are getting rid of msrp logic because of its inaccuracy. We will be keep the fields though for consistency
+    --Keeping order_gross_revenue_product = order_gross_revenue_product_shopfiy for consistency
+    sum(iff(is_vendor_route = false, oli.order_line_item_price, 0) * oli.order_line_item_units) over (partition by o.order_id) as order_gross_revenue_product,
     sum(iff(is_vendor_route = false, oli.order_line_item_price, 0) * oli.order_line_item_units) over (partition by o.order_id) as order_gross_revenue_product_shopify,
     sum(oli.order_line_item_price_route) over (partition by o.order_id) * order_units_route as order_gross_revenue_route,
     o.total_tax as order_gross_revenue_tax,
     round(o.total_price - o.subtotal_price - o.total_tax, 2) as order_gross_revenue_shipping,
     order_gross_revenue_product + order_gross_revenue_route + order_gross_revenue_tax + order_gross_revenue_shipping as order_gross_revenue_total,
-    o.total_discounts + sum((nvl(i.msrp, oli.order_line_item_price) - oli.order_line_item_price) * oli.order_line_item_units) over (partition by o.order_id) as order_discount,
+    o.total_discounts as order_discount,
     round(o.total_price - o.current_total_price, 2) as order_refund,
 
     --we might be missing the order shipping discount. This can be calculated. 
@@ -32,11 +34,11 @@ select
     max(o.current_total_price) over (partition by o.order_id) as order_net_revenue_total,
     oli.order_line_item_vendor,
     oli.order_line_item_price as order_line_item_price_shopify,
-    nvl(i.msrp, oli.order_line_item_price) as order_line_item_msrp,
+    oli.order_line_item_price as order_line_item_msrp,
     oli.order_line_item_units,
     order_line_item_msrp * oli.order_line_item_units as order_line_item_gross_revenue,
     oli.order_line_item_price * oli.order_line_item_units as order_line_item_gross_revenue_shopify,
-    ((order_line_item_msrp - oli.order_line_item_price) * oli.order_line_item_units) + oli.total_discount as order_line_item_total_discount,    
+    oli.total_discount as order_line_item_total_discount,    
     nvl(oli.sku, p.product_sku) as order_line_item_sku,
     p.product_barcode,
     oli.is_gift_card as order_line_item_is_gift_card,
@@ -57,6 +59,7 @@ left join {{ref('dim_date_ad')}} as d
     on d."DATE" = o.order_placed_at_utc::date
 {# left join {{ref('dim_discount_ad')}} as discount 
     on discount.order_id = o.order_id #}
-left join {{ ref('int_historic_msrp_ad') }} as i 
+ --The join with int_historic_msrp table is not needed for American Duchess since we are not using that here.
+{# left join {{ ref('int_historic_msrp_ad') }} as i 
     on oli.product_title = i.product_title
-    and o.order_placed_at_utc::date = i.order_date
+    and o.order_placed_at_utc::date = i.order_date #}
