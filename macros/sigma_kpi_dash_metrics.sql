f84034b4bd1bf3d0b1a8f2a3a04c2e0b4abcc973
caroline@aestuary.com <caroline@aestuary.com>
2023-04-05 21:58:43 +0000

    add dashboard marketing reporting table (different LOD than order_by_reporting_table so i built it separately

--- macros/sigma_kpi_dash_metrics.sql
+++ macros/sigma_kpi_dash_metrics.sql
@@ -45,7 +45,6 @@
   , avg(case when o.is_new_customer_order then o.net_revenue_total end) as AOV_net_total_new_customers
   , avg(case when not o.is_new_customer_order then o.net_revenue_total end) as AOV_net_total_recur_customers
 
-
 {%- endmacro %}
 
 {% macro insert_kpi_comparison_metrics() -%}
@@ -166,5 +165,74 @@
     , ty.aov_net_total_recur_customers
     , ly.aov_net_total_recur_customers as ly_aov_net_total_recur_customers
     , (ty.aov_net_total_recur_customers/nullif(ly.aov_net_total_recur_customers,0))-1 as yoy_aov_net_total_recur_customers
+
+
+{%- endmacro %}
+
+{% macro insert_marketing_kpi_metrics() -%}
+
+  /*Marketing metrics*/
+  , sum(m.impressions_fb) as impressions_fb
+  , sum(m.clicks_fb) as clicks_fb
+  , sum(m.clicks_fb) / sum(nullif(m.impressions_fb,0)) as ctr_fb
+  , sum(m.spend_fb) as spend_fb
+  , sum(m.reach_fb) as reach_fb
+  , sum(m.impressions_goog) as impressions_goog
+  , sum(m.clicks_goog) as clicks_goog
+  , sum(m.clicks_goog) / sum(nullif(m.impressions_goog,0)) as ctr_goog
+  , sum(m.spend_goog) as spend_goog
+  , sum(m.reach_goog) as reach_goog
+  , sum(ifnull(m.impressions_fb,0) + ifnull(m.impressions_goog,0)) as impressions_total
+  , sum(ifnull(m.clicks_fb,0) + ifnull(m.clicks_goog,0)) as clicks_total
+  , sum(ifnull(m.spend_fb,0) + ifnull(m.spend_goog,0)) as spend_total
+  , sum(ifnull(m.reach_fb,0) + ifnull(m.reach_goog,0)) as reach_total
+
+{%- endmacro %}
+
+{% macro insert_marketing_kpi_comparison_metrics() -%}
+
+
+    , ty.impressions_fb
+    , ly.impressions_fb as ly_impressions_fb
+    , (ty.impressions_fb/nullif(ly.impressions_fb,0))-1 as yoy_impressions_fb
+    , ty.clicks_fb
+    , ly.clicks_fb as ly_clicks_fb
+    , (ty.clicks_fb/nullif(ly.clicks_fb,0))-1 as yoy_clicks_fb
+    , ty.ctr_fb
+    , ly.ctr_fb as ly_ctr_fb
+    , (ty.ctr_fb/nullif(ly.ctr_fb,0))-1 as yoy_ctr_fb
+    , ty.spend_fb
+    , ly.spend_fb as ly_spend_fb
+    , (ty.spend_fb/nullif(ly.spend_fb,0))-1 as yoy_spend_fb
+    , ty.reach_fb
+    , ly.reach_fb as ly_reach_fb
+    , (ty.reach_fb/nullif(ly.reach_fb,0))-1 as yoy_reach_fb
+    , ty.impressions_goog
+    , ly.impressions_goog as ly_impressions_goog
+    , (ty.impressions_goog/nullif(ly.impressions_goog,0))-1 as yoy_impressions_goog
+    , ty.clicks_goog
+    , ly.clicks_goog as ly_clicks_goog
+    , (ty.clicks_goog/nullif(ly.clicks_goog,0))-1 as yoy_clicks_goog
+    , ty.ctr_goog
+    , ly.ctr_goog as ly_ctr_goog
+    , (ty.ctr_goog/nullif(ly.ctr_goog,0))-1 as yoy_ctr_goog
+    , ty.spend_goog
+    , ly.spend_goog as ly_spend_goog
+    , (ty.spend_goog/nullif(ly.spend_goog,0))-1 as yoy_spend_goog
+    , ty.reach_goog
+    , ly.reach_goog as ly_reach_goog
+    , (ty.reach_goog/nullif(ly.reach_goog,0))-1 as yoy_reach_goog
+    , ty.impressions_total
+    , ly.impressions_total as ly_impressions_total
+    , (ty.impressions_total/nullif(ly.impressions_total,0))-1 as yoy_impressions_total
+    , ty.clicks_total
+    , ly.clicks_total as ly_clicks_total
+    , (ty.clicks_total/nullif(ly.clicks_total,0))-1 as yoy_clicks_total
+    , ty.spend_total
+    , ly.spend_total as ly_spend_total
+    , (ty.spend_total/nullif(ly.spend_total,0))-1 as yoy_spend_total
+    , ty.reach_total
+    , ly.reach_total as ly_reach_total
+    , (ty.reach_total/nullif(ly.reach_total,0))-1 as yoy_reach_total
 
 {%- endmacro %}
