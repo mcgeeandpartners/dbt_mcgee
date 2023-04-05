ce2c47967e0ba4b115a01570af7f977e4f86e3bd
caroline@aestuary.com <caroline@aestuary.com>
2023-04-05 20:44:40 +0000

    add recurring customer metrics to kpi metrics macro

--- macros/sigma_kpi_dash_metrics.sql
+++ macros/sigma_kpi_dash_metrics.sql
@@ -4,35 +4,47 @@
   /*Customer metrics*/
   , count(distinct o.customer_id) as unique_customers
   , count(distinct case when o.is_new_customer_order then o.customer_id end) as unique_new_customers
+  , count(distinct case when not o.is_new_customer_order then o.customer_id end) as unique_recur_customers
   , median(o.customer_months_since_acq) as median_age_customer_months_since_acq
   , avg(o.customer_months_since_acq) as avg_age_customer_months_since_acq
   /*Orders metrics*/
   , count(o.order_id) as orders
   , sum(case when o.is_new_customer_order then 1 end) as orders_new_customers
+  , sum(case when not o.is_new_customer_order then 1 end) as orders_recur_customers
   , sum(o.units_product) as units_product
   , sum(o.units_route) as units_route
   /*Financial metrics*/
   , sum(o.gross_revenue_product) as gross_revenue_product
   , sum(o.gross_revenue_product - o.discount_total) as net_revenue_product
   , sum(case when o.is_new_customer_order then o.gross_revenue_product end) as gross_revenue_product_new_customers
+  , sum(case when not o.is_new_customer_order then o.gross_revenue_product end) as gross_revenue_product_recur_customers 
   , sum(case when o.is_new_customer_order then o.gross_revenue_product - o.discount_total end) as net_revenue_product_new_customers
+  , sum(case when not o.is_new_customer_order then o.gross_revenue_product - o.discount_total end) as net_revenue_product_recur_customers
   , sum(o.gross_revenue_route) as gross_revenue_route
   , sum(o.gross_revenue_shipping) as gross_revenue_shipping
   , sum(o.gross_revenue_tax) as gross_revenue_tax
   , sum(o.gross_revenue_total) as gross_revenue_total
   , avg(o.gross_revenue_total) as AOV_gross_total
   , sum(case when o.is_new_customer_order then o.gross_revenue_total end) as gross_revenue_total_new_customers
+  , sum(case when not o.is_new_customer_order then o.gross_revenue_total end) as gross_revenue_total_recur_customers
   , avg(case when o.is_new_customer_order then o.gross_revenue_total end) as AOV_gross_total_new_customers
+  , avg(case when not o.is_new_customer_order then o.gross_revenue_total end) as AOV_gross_total_recur_customers  
   , sum(o.discount_total) as discounts_total
   , sum(o.discount_total)/sum(o.gross_revenue_total) as discounts_percent
   , sum(case when o.is_new_customer_order then o.discount_total end) as discounts_total_new_customers
   , sum(case when o.is_new_customer_order then o.discount_total end)
         /sum(case when o.is_new_customer_order then o.gross_revenue_total end) as discounts_percent_new_customers
+  , sum(case when not o.is_new_customer_order then o.discount_total end) as discounts_total_recur_customers
+  , sum(case when not o.is_new_customer_order then o.discount_total end)
+        /sum(case when not o.is_new_customer_order then o.gross_revenue_total end) as discounts_percent_recur_customers
   , sum(o.refunds_total) as refunds_total
   , sum(o.net_revenue_total) as net_revenue_total
   , sum(case when o.is_new_customer_order then o.net_revenue_total end) as net_revenue_total_new_customers
+  , sum(case when not o.is_new_customer_order then o.net_revenue_total end) as net_revenue_total_recur_customers 
   , avg(o.net_revenue_total) as AOV_net_total
   , avg(case when o.is_new_customer_order then o.net_revenue_total end) as AOV_net_total_new_customers
+  , avg(case when not o.is_new_customer_order then o.net_revenue_total end) as AOV_net_total_recur_customers
+
 
 {%- endmacro %}
 
@@ -44,6 +56,9 @@
     , ty.unique_new_customers
     , ly.unique_new_customers as ly_unique_new_customers
     , (ty.unique_new_customers/nullif(ly.unique_new_customers,0))-1 as yoy_unique_new_customers
+    , ty.unique_recur_customers
+    , ly.unique_recur_customers as ly_unique_recur_customers
+    , (ty.unique_recur_customers/nullif(ly.unique_recur_customers,0))-1 as yoy_unique_recur_customers    
     , ty.median_age_customer_months_since_acq
     , ly.median_age_customer_months_since_acq as ly_median_age_customer_months_since_acq
     , (ty.median_age_customer_months_since_acq/nullif(ly.median_age_customer_months_since_acq,0))-1 as yoy_median_age_customer_months_since_acq
@@ -57,6 +72,9 @@
     , ty.orders_new_customers
     , ly.orders_new_customers as ly_orders_new_customers
     , (ty.orders_new_customers/nullif(ly.orders_new_customers,0))-1 as yoy_orders_new_customers
+    , ty.orders_recur_customers
+    , ly.orders_recur_customers as ly_orders_recur_customers
+    , (ty.orders_recur_customers/nullif(ly.orders_recur_customers,0))-1 as yoy_orders_recur_customers
     , ty.units_product
     , ly.units_product as ly_units_product
     , (ty.units_product/nullif(ly.units_product,0))-1 as yoy_units_product
@@ -73,9 +91,15 @@
     , ty.gross_revenue_product_new_customers
     , ly.gross_revenue_product_new_customers as ly_gross_revenue_product_new_customers
     , (ty.gross_revenue_product_new_customers/nullif(ly.gross_revenue_product_new_customers,0))-1 as yoy_gross_revenue_product_new_customers
+    , ty.gross_revenue_product_recur_customers
+    , ly.gross_revenue_product_recur_customers as ly_gross_revenue_product_recur_customers
+    , (ty.gross_revenue_product_recur_customers/nullif(ly.gross_revenue_product_recur_customers,0))-1 as yoy_gross_revenue_product_recur_customers
     , ty.net_revenue_product_new_customers
     , ly.net_revenue_product_new_customers as ly_net_revenue_product_new_customers
     , (ty.net_revenue_product_new_customers/nullif(ly.net_revenue_product_new_customers,0))-1 as yoy_net_revenue_product_new_customers
+   , ty.net_revenue_product_recur_customers
+    , ly.net_revenue_product_recur_customers as ly_net_revenue_product_recur_customers
+    , (ty.net_revenue_product_recur_customers/nullif(ly.net_revenue_product_recur_customers,0))-1 as yoy_net_revenue_product_recur_customers
     , ty.gross_revenue_route
     , ly.gross_revenue_route as ly_gross_revenue_route
     , (ty.gross_revenue_route/nullif(ly.gross_revenue_route,0))-1 as yoy_gross_revenue_route
@@ -94,9 +118,15 @@
     , ty.gross_revenue_total_new_customers
     , ly.gross_revenue_total_new_customers as ly_gross_revenue_total_new_customers
     , (ty.gross_revenue_total_new_customers/nullif(ly.gross_revenue_total_new_customers,0))-1 as yoy_gross_revenue_total_new_customers
+    , ty.gross_revenue_total_recur_customers
+    , ly.gross_revenue_total_recur_customers as ly_gross_revenue_total_recur_customers
+    , (ty.gross_revenue_total_recur_customers/nullif(ly.gross_revenue_total_recur_customers,0))-1 as yoy_gross_revenue_total_recur_customers
     , ty.aov_gross_total_new_customers
     , ly.aov_gross_total_new_customers as ly_aov_gross_total_new_customers
     , (ty.aov_gross_total_new_customers/nullif(ly.aov_gross_total_new_customers,0))-1 as yoy_aov_gross_total_new_customers
+    , ty.aov_gross_total_recur_customers
+    , ly.aov_gross_total_recur_customers as ly_aov_gross_total_recur_customers
+    , (ty.aov_gross_total_recur_customers/nullif(ly.aov_gross_total_recur_customers,0))-1 as yoy_aov_gross_total_recur_customers
     , ty.discounts_total
     , ly.discounts_total as ly_discounts_total
     , (ty.discounts_total/nullif(ly.discounts_total,0))-1 as yoy_discounts_total
@@ -109,6 +139,12 @@
     , ty.discounts_percent_new_customers
     , ly.discounts_percent_new_customers as ly_discounts_percent_new_customers
     , (ty.discounts_percent_new_customers/nullif(ly.discounts_percent_new_customers,0))-1 as yoy_discounts_percent_new_customers
+    , ty.discounts_total_recur_customers
+    , ly.discounts_total_recur_customers as ly_discounts_total_recur_customers
+    , (ty.discounts_total_recur_customers/nullif(ly.discounts_total_recur_customers,0))-1 as yoy_discounts_total_recur_customers
+    , ty.discounts_percent_recur_customers
+    , ly.discounts_percent_recur_customers as ly_discounts_percent_recur_customers
+    , (ty.discounts_percent_recur_customers/nullif(ly.discounts_percent_recur_customers,0))-1 as yoy_discounts_percent_recur_customers
     , ty.refunds_total
     , ly.refunds_total as ly_refunds_total
     , (ty.refunds_total/nullif(ly.refunds_total,0))-1 as yoy_refunds_total
@@ -118,11 +154,17 @@
     , ty.net_revenue_total_new_customers
     , ly.net_revenue_total_new_customers as ly_net_revenue_total_new_customers
     , (ty.net_revenue_total_new_customers/nullif(ly.net_revenue_total_new_customers,0))-1 as yoy_net_revenue_total_new_customers
+    , ty.net_revenue_total_recur_customers
+    , ly.net_revenue_total_recur_customers as ly_net_revenue_total_recur_customers
+    , (ty.net_revenue_total_recur_customers/nullif(ly.net_revenue_total_recur_customers,0))-1 as yoy_net_revenue_total_recur_customers
     , ty.aov_net_total
     , ly.aov_net_total as ly_aov_net_total
     , (ty.aov_net_total/nullif(ly.aov_net_total,0))-1 as yoy_aov_net_total
     , ty.aov_net_total_new_customers
     , ly.aov_net_total_new_customers as ly_aov_net_total_new_customers
     , (ty.aov_net_total_new_customers/nullif(ly.aov_net_total_new_customers,0))-1 as yoy_aov_net_total_new_customers
+    , ty.aov_net_total_recur_customers
+    , ly.aov_net_total_recur_customers as ly_aov_net_total_recur_customers
+    , (ty.aov_net_total_recur_customers/nullif(ly.aov_net_total_recur_customers,0))-1 as yoy_aov_net_total_recur_customers
 
 {%- endmacro %}
