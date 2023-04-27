224bd141c59d2016e39ea4d865d65bae5278f149
Ovais Siddiqui <muhd.ovaissiddiqui@gmail.com>
2023-04-27 09:41:29 -0500

    add new skus table

--- models/shopify_alice_ames/6_reporting/1_base/base_rpt_new_skus.sql
+++ models/shopify_alice_ames/6_reporting/1_base/base_rpt_new_skus.sql
@@ -2,12 +2,16 @@
 
 {{
   config(
-    materialized = 'view',
+    materialized = 'view'
     )
 }}
 
-select distinct a.product_variant_name, a.product_title
+select 
+    a.product_variant_name, a.product_title, min(c.order_placed_at_utc) as initial_order_placed_at_utc
 from {{ ref('transform_order_line_item_alice_ames') }} as a 
+inner join {{ ref('stg_order_alice_ames') }} as c 
+    on a.order_id = c.order_id    
 left join {{ ref('stg_master_sku_list_alice_ames') }} as b 
     on a.product_variant_name = b.product_variant_name
 where b.product_variant_name is null
+group by 1, 2
