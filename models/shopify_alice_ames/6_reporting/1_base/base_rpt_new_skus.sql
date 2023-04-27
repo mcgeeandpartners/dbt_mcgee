696c9ff55add5bd0b1e31b4b50dee39047b8da32
Ovais Siddiqui <muhd.ovaissiddiqui@gmail.com>
2023-04-26 23:13:51 -0500

    change materialization

--- models/shopify_alice_ames/6_reporting/1_base/base_rpt_new_skus.sql
+++ models/shopify_alice_ames/6_reporting/1_base/base_rpt_new_skus.sql
@@ -1,5 +1,11 @@
 --Variant names that havent been added to the sku list yet
 
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
 select distinct a.product_variant_name, a.product_title
 from {{ ref('transform_order_line_item_alice_ames') }} as a 
 left join {{ ref('stg_master_sku_list_alice_ames') }} as b 
