d1a6b2c1a16025d46b88d11df9807428cd78c081
Ovais Siddiqui <muhd.ovaissiddiqui@gmail.com>
2023-05-04 12:14:17 -0500

    Add master sku list to products

--- models/shopify_american_duchess/4_dim/dim_product_ad.sql
+++ models/shopify_american_duchess/4_dim/dim_product_ad.sql
@@ -1,17 +1,5 @@
 {{ config(alias="dim_product") }}
 
-{# with variant_name_id_mapping as (
-    select m.product_variant_name, m.product_title, oli.product_variant_id, order_line_item_id
-    from {{ ref('stg_master_sku_list_tenkara_usa') }} as m
-    left join {{ ref('transform_order_line_item_tenkara_usa') }} as oli 
-    	on m.product_variant_name = oli.product_variant_name
-        and m.product_title = oli.product_title
-    --a product variant and proudct title combo can belong to >1 distinct varinat ids. So we dedupe them using qualify. It doesnt really affect anything. We just need to dedepe them.         
-    qualify row_number() over (partition by m.product_title, oli.product_variant_id order by oli.order_line_item_id) = 1
-    ) #}
-
-
-
 select
     pv.product_id,
     pv.product_variant_id,
@@ -36,15 +24,18 @@ select
     pv.compare_at_price,
     pv.created_at_utc as product_created_at,
     pv.updated_at_utc as product_updated_at,
-    {# these fields need to be updated when we have the SKU mapping document #}
-    NULL as product_category,
-    NULL as product_sub_type,
-    NULL as product_heel,
-    NULL as product_era,
-    NULL as product_genre,
-    NULL as product_decade,
-    NULL as product_is_pre_order,
-    NULL as product_color,
+    v.volume_proxy, 
+    v.product_title_adj, 
+    v.product_category, 
+    v.product_type, 
+    v.product_sub_type, 
+    v.product_heel, 
+    v.product_era, 
+    v.product_genre, 
+    v.product_decade, 
+    v.product_color, 
+    v.product_style, 
+    v.product_is_pre_order, 
     NULL as product_made_in,
     NULL as product_manufacturer,
     i.inventory_item_id, 
@@ -56,11 +47,8 @@ from {{ ref('transform_product_ad') }} as p
 left join {{ ref('transform_product_variant_ad') }} as pv 
     on p.product_id = pv.product_id
 --Following is for master SKU list fields
-{# left join variant_name_id_mapping as v 
-    on pv.product_variant_id = v.product_variant_id
-    and v.product_title = p.product_title
-left join {{ ref('stg_master_sku_list_alice_ames') }} as m 
-    on v.product_variant_name = m.product_variant_name #}
+left join {{ ref('stg_master_sku_list_ad') }} as v
+    on pv.product_variant_name = v.product_variant_name
 --Get inventory levels
 left join {{ ref('stg_inventory_item_level_ad')}}  as i
     on nullif(lower(pv.product_variant_sku), '') = i.product_sku
