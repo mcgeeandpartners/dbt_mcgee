e1c666d2652a8ee7d1138cddbd0b584d9121a996
caroline@aestuary.com <caroline@aestuary.com>
2023-05-23 17:10:38 +0000

    add ad eu to downstream consolidated reporting tables

--- models/consolidated_bi_reports/dash_consolidated_by_reporting_period_all.sql
+++ models/consolidated_bi_reports/dash_consolidated_by_reporting_period_all.sql
@@ -2,6 +2,8 @@
 
 SELECT 'American Duchess' as sub_company, * FROM {{ ref('dash_consolidated_by_reporting_period_ad') }}
 UNION
+SELECT 'American Duchess - EU' as sub_company, * FROM {{ ref('dash_consolidated_by_reporting_period_ad_eu') }}
+UNION
 SELECT 'Alice and Ames' as sub_company, * FROM {{ ref('dash_consolidated_by_reporting_period_alice_ames') }}
 UNION
 SELECT 'Tenkara USA' as sub_company, * FROM {{ ref('dash_consolidated_by_reporting_period_tenkara_usa') }}
