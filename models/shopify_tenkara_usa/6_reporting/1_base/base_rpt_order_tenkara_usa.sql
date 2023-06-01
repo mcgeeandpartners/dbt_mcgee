53dbdaa4d08565386293a7aeedf5ae7e733032c5
caroline@aestuary.com <caroline@aestuary.com>
2023-06-01 17:07:27 +0000

    make macro and add logic for other brands

--- models/shopify_tenkara_usa/6_reporting/1_base/base_rpt_order_tenkara_usa.sql
+++ models/shopify_tenkara_usa/6_reporting/1_base/base_rpt_order_tenkara_usa.sql
@@ -37,23 +37,9 @@ select
   , mkt_attri.utm_source
   , oli.landing_site_base_url
   , oli.referring_site
-  , CASE WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'google') THEN 'Google'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'linkin.bio') THEN 'Meta'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'instagram') THEN 'Meta'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'facebook') THEN 'Meta'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'snappic') THEN 'Meta'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'retailmenot') THEN 'Direct'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'Newsletter') THEN 'Email'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'attentive') THEN 'SMS'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'IGShopping') THEN 'Meta'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'retailmenot') THEN 'Direct'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'smsbump-campaigns') THEN 'SMS'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'smsbump-flows') THEN 'SMS'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'bing') THEN 'Bing'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'aliceandames.com') THEN 'Direct'
-    WHEN Contains(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct'), 'duckduckgo') THEN 'Direct'
-    ELSE Coalesce(mkt_attri.utm_source, case when length(oli.referring_site) = 0 then NULL else oli.referring_site end, 'Direct')
-    END as Shopify_Last_Click_Attr
+  
+  {{ insert_shopify_last_click_attr_metric() }}
+  
   /*Customer Fact Fields*/
   , datediff("month", customers.customer_cohort_month, dates.date) as customer_months_since_acq
   /*Order Fact Fields*/
