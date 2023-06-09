a42a2c0be87e3a12f5412794ac217b01ba446f8f
caroline@aestuary.com <caroline@aestuary.com>
2023-06-09 19:23:19 +0000

    edit shopify attr logic

--- macros/sigma_mkt_metrics.sql
+++ macros/sigma_mkt_metrics.sql
@@ -1,26 +1,36 @@
 {% macro insert_shopify_last_click_attr_metric() -%}
 
-, CASE WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'google') THEN 'Google'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'linkin.bio') THEN 'Meta'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'instagram') THEN 'Meta'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'facebook') THEN 'Meta'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'snappic') THEN 'Meta'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'retailmenot') THEN 'Direct'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'Newsletter') THEN 'Email'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'attentive') THEN 'SMS'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'IGShopping') THEN 'Meta'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'pinterest') THEN 'Meta'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'retailmenot') THEN 'Direct'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'smsbump-campaign') THEN 'SMS'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'smsbump-flow') THEN 'SMS'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'bing') THEN 'Bing'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'yahoo') THEN 'Yahoo'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'amazon') THEN 'Amazon'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'aliceandames') THEN 'Direct'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'americanduchess') THEN 'Direct'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'littlepoppyco') THEN 'Direct'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'tenkarausa') THEN 'Direct'
-    WHEN Contains(lower(Coalesce(mkt_attri.utm_source, oli.referring_site, 'Direct')), 'duckduckgo') THEN 'Direct'
-    ELSE Coalesce(mkt_attri.utm_source, case when length(oli.referring_site) = 0 then NULL else 'Other' end, 'Direct')
+, CASE 
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'google') THEN 'Google'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'linkin.bio')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'facebook')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'instagram')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'snappic')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'igshopping')
+        THEN 'Meta'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'pinterest') THEN 'Pinterest'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'email') THEN 'Email'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'afterpay') THEN 'Afterpay'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'disco') THEN 'Disco'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'bing') THEN 'Bing'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'yahoo') THEN 'Yahoo'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'amazon') THEN 'Amazon'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'email')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'newsletter')
+        THEN 'Email'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'klaviyo') THEN 'Klaviyo'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'retailmenot')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'aliceandames')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'americanduchess')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'littlepoppyco')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'tenkarausa')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'duckduckgo')
+        THEN 'Direct'
+    WHEN Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'attentive')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'smsbump-campaign')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'smsbump-flow')
+        OR Contains(lower(concat(ifnull(mkt_attri.utm_medium,''),ifnull(mkt_attri.utm_source,''),ifnull(oli.referring_site,''))),'sms')
+        THEN 'SMS'    
+    ELSE 'Other'
     END as Shopify_Last_Click_Attr
 {%- endmacro %}
