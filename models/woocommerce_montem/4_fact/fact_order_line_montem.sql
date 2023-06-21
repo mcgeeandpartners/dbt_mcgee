50bfa51dc3191d998c55da95f92da9b7e7181963
Lenin Mishra <pylenin1992@gmail.com>
2023-06-21 21:34:53 +0530

    Fix volumn names for Montem fact tables

--- models/woocommerce_montem/4_fact/fact_order_line_montem.sql
+++ models/woocommerce_montem/4_fact/fact_order_line_montem.sql
@@ -22,6 +22,7 @@ with
     new_oli_agg as (
         select
             oli.order_id,
+            oli.id as order_line_item_id,
             sum(
                 case
                     when oli.order_line_item_vendor = 'Montem'
@@ -51,7 +52,7 @@ with
                 end
             ) as order_gross_revenue_route
         from new_order_line_item oli
-        group by 1
+        group by 1, 2
     ),
     refunds_agg as (
         select order_refund.order_id, sum(refund.amount) as refunded_amount
@@ -61,17 +62,20 @@ with
             on order_refund.refund_id = refund.id
         group by 1
     )
-
 select
-    o.*,
+    o.id as order_id,
     c.customer_key,
     d.date_key,
+    null as product_key,
     null as order_cancel_reason, 
     null as order_financial_status, 
     null as order_fulfillment_status, 
+    oli_agg.order_line_item_id,
     o.currency as order_currency,
     o.prices_include_tax as order_has_tax_included,
     null as is_new_customer_order, 
+    null as landing_site_base_url,
+    null as referring_site,
     oli_agg.order_units_product,
     oli_agg.order_units_route,
     oli_agg.order_gross_revenue_product,
@@ -84,7 +88,17 @@ select
     + o.shipping_total as order_gross_revenue_total,
     o.discount_total as order_discount,
     refunds_agg.refunded_amount as order_refund,
-    o.total - refunds_agg.refunded_amount as order_net_revenue_total
+    o.total - refunds_agg.refunded_amount as order_net_revenue_total,
+    NULL AS ORDER_LINE_ITEM_UNITS,
+    NULL AS ORDER_LINE_ITEM_IS_GIFT_CARD,
+    NULL AS ORDER_LINE_ITEM_MSRP,
+    NULL AS PRODUCT_BARCODE,
+    NULL AS ORDER_LINE_ITEM_SKU,
+    NULL AS ORDER_LINE_ITEM_TOTAL_DISCOUNT,
+    NULL as ORDER_LINE_ITEM_IS_TAXABLE,
+    NULL AS ORDER_LINE_ITEM_GROSS_REVENUE,
+    NULL as ORDER_LINE_ITEM_PRICE_MONTEM,
+    NULL as order_line_item_vendor
 from {{ref('stg_order_montem')}} o
 left join {{ref('dim_customer_montem')}} as c 
     on o.customer_id = c.customer_id
