c7e0e555bef3d550f71fba60e121085cd3266ac0
Julian Enns <julianenns7@gmail.com>
2024-08-26 16:15:54 -0600

    Update financials_impress.sql

--- models/impress/xero/financials_impress.sql
+++ models/impress/xero/financials_impress.sql
@@ -44,7 +44,7 @@ left join
 left join
     {{ source("impress_sp", "coa_metadata") }} psp
     on lower(psp.account_id) = lower(jl.account_id)
-where tc.name='Clients' or tc.name is NULL 
+where tc.name='Client' or tc.name is NULL 
 )
 ,
   impress_division as (
@@ -71,7 +71,7 @@ where tc.name='Clients' or tc.name is NULL
    -- tcho.tracking_option_id,
    -- tco.name as customer_name,
    -- tc.name as tracking_category_name,
-     tco.name as Division
+     tco.name as Directors
 
 from {{ source("impress", "journal") }} j
 left join {{ source("impress", "journal_line") }} jl on j.journal_id = jl.journal_id
@@ -93,7 +93,7 @@ left join
 left join
     {{ source("impress_sp", "coa_metadata") }} psp
     on lower(psp.account_id) = lower(jl.account_id)
-where tc.name='Division'
+where tc.name='Directors'
 ),
 impress as (
     select 
@@ -117,7 +117,7 @@ impress as (
    -- COALESCE(pd.tracking_option_id, pc.tracking_option_id) AS tracking_option_id,
    -- COALESCE(pd.customer_name, pc.customer_name) AS customer_name,
    -- COALESCE(pd.tracking_category_name, pc.tracking_category_name) AS tracking_category_name,
-    pd.Division,
+    pd.Directors,
 	pc.Client
      from impress_clients pc
     full outer join impress_division  pd on pc.journal_line_id=pd.journal_line_id 
@@ -139,15 +139,15 @@ impress as (
     psp.account_category, 
     psp.account_report, 
     psp.account_class,
-    psp.working_apital_ as  working_apital,
+    psp.working_apital_,
     'PEARSHOP' as entity,
     -- jlt.tracking_category_id,
     -- jlt.option,
     -- tcho.tracking_option_id,
     -- tco.name as customer_name,
     -- tc.name as tracking_category_name,
-     NULL as Division,
-     tco.name as Clients 
+    -- NULL as Director,
+     tco.name as Client
 
 from {{ source("pearshop", "journal") }} j
 left join {{ source("pearshop", "journal_line") }} jl on j.journal_id = jl.journal_id
@@ -169,7 +169,82 @@ left join
 left join
     {{ source("impress_sp", "coa_metadata") }} psp
     on lower(psp.account_id) = lower(jl.account_id)
-where tc.name='Clients' or tc.name is null
+where tc.name='Client' or tc.name is null
+),
+pearshop_division as (
+    select
+    jl.journal_line_id, 
+    jl.account_id, 
+    jl.account_code, 
+    jl.account_type, 
+    jl.account_name, 
+    jl.net_amount  as net_amount_original,  
+    CASE 
+    WHEN jl.account_type in ('REVENUE','DIRECTCOSTS')THEN jl.net_amount * -1  
+    ELSE jl.net_amount * 1
+    END AS net_amount, 
+    j.journal_id, 
+    j.journal_date, 
+    psp.account_category, 
+    psp.account_report, 
+    psp.account_class,
+    psp.working_apital_,
+    'IMPRESS' as entity,
+   -- jlt.tracking_category_id,
+   -- jlt.option,
+   -- tcho.tracking_option_id,
+   -- tco.name as customer_name,
+   -- tc.name as tracking_category_name,
+     tco.name as Directors
+
+from {{ source("pearshop", "journal") }} j
+left join {{ source("pearshop", "journal_line") }} jl on j.journal_id = jl.journal_id
+left join
+    {{ source("pearshop", "journal_line_has_tracking_category") }} jlt
+    on jl.journal_line_id = jlt.journal_line_id
+    and jl.journal_id = jlt.journal_id
+left join
+    {{ source("pearshop", "tracking_category_has_option") }} tcho
+    on jlt.tracking_category_id = tcho.tracking_category_id
+    and jlt.tracking_category_option_id = tcho.tracking_option_id
+left join
+    {{ source("pearshop", "tracking_category_option") }} tco
+    on tcho.tracking_option_id = tco.tracking_option_id
+left join
+    {{ source("pearshop", "tracking_category") }} tc
+    on tc.tracking_category_id = jlt.tracking_category_id
+
+left join
+    {{ source("impress_sp", "coa_metadata") }} psp
+    on lower(psp.account_id) = lower(jl.account_id)
+where tc.name='Directors'
+),
+pearshop as (
+    select 
+        COALESCE(rd.journal_line_id, rc.journal_line_id) AS journal_line_id,
+    COALESCE(rd.account_id, rc.account_id) AS account_id,
+    COALESCE(rd.account_code, rc.account_code) AS account_code,
+    COALESCE(rd.account_type, rc.account_type) AS account_type,
+    COALESCE(rd.account_name, rc.account_name) AS account_name,
+    COALESCE(rd.net_amount_original,rc.net_amount_original) as net_amount_original,
+    COALESCE(rd.net_amount, rc.net_amount) AS net_amount,
+    COALESCE(rd.journal_id, rc.journal_id) AS journal_id,
+    COALESCE(rd.journal_date, rc.journal_date) AS journal_date,
+    COALESCE(rd.account_category, rc.account_category) AS account_category,
+    COALESCE(rd.account_report, rc.account_report) AS account_report,
+    COALESCE(rd.account_class, rc.account_class) AS account_class,
+    COALESCE(rd.working_apital_, rc.working_apital_) AS working_apital,
+    'PEARSHOP' AS entity,
+
+   -- COALESCE(pd.tracking_category_id, pc.tracking_category_id) AS tracking_category_id,
+   -- COALESCE(pd.option, pc.option) AS option,
+   -- COALESCE(pd.tracking_option_id, pc.tracking_option_id) AS tracking_option_id,
+   -- COALESCE(pd.customer_name, pc.customer_name) AS customer_name,
+   -- COALESCE(pd.tracking_category_name, pc.tracking_category_name) AS tracking_category_name,
+    rd.Directors,
+	rc.Client
+     from pearshop_clients rc
+    full outer join pearshop_division  rd on rc.journal_line_id=rd.journal_line_id 
 )
 select pl.*, dd.fiscal_year, dd.fiscal_quarter,dd.yearmonth from impress pl
 
@@ -177,7 +252,7 @@ left join {{ ref("dim_date") }} dd on pl.journal_date::date = dd.date
 
 union all
 
-select rc.*, dd.fiscal_year, dd.fiscal_quarter,dd.yearmonth from pearshop_clients rc
+select rl.*, dd.fiscal_year, dd.fiscal_quarter,dd.yearmonth from pearshop rl
 
-left join {{ ref("dim_date") }} dd on rc.journal_date::date = dd.date
+left join {{ ref("dim_date") }} dd on rl.journal_date::date = dd.date
 
