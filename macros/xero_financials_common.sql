923201171766f6388f9c88eba713a7f76feb440d
taha dar <tahadar422@gmail.com>
2024-09-02 06:10:47 +0500

    common macro for xero financials

---
+++ macros/xero_financials_common.sql
@@ -0,0 +1,73 @@
+
+{% macro xero_financials_common(dict_data) %}
+
+
+with  
+{% for cte_name,cte_category in dict_data.items() %}
+{{cte_name}}_combined as
+   ( select
+        jl.journal_line_id, 
+        jl.account_id, 
+        jl.account_code, 
+        jl.account_type, 
+        jl.account_name, 
+        jl.net_amount as net_amount_original,  
+        CASE 
+            WHEN jl.account_type in ('REVENUE','OTHERINCOME') THEN jl.net_amount * -1  
+            ELSE jl.net_amount * 1
+        END AS net_amount,
+        j.journal_id, 
+        j.journal_date, 
+        psp.account_category, 
+        psp.account_report, 
+        psp.account_class,
+        psp.working_apital_,
+        '{{cte_name}}' as entity,
+   {% for cc in cte_category %}
+        CASE 
+            WHEN tc.name = '{{cc}}' THEN tco.name 
+            ELSE NULL 
+        END AS {{cc.rstrip('s')}}
+
+        {%if not loop.last %} , {% endif %}
+        {% endfor %}
+
+    from {{ source(cte_name, "journal") }} j
+    left join {{ source(cte_name, "journal_line") }} jl on j.journal_id = jl.journal_id
+    left join {{ source(cte_name, "journal_line_has_tracking_category") }} jlt
+        on jl.journal_line_id = jlt.journal_line_id
+        and jl.journal_id = jlt.journal_id
+    left join {{ source(cte_name, "tracking_category_has_option") }} tcho
+        on jlt.tracking_category_id = tcho.tracking_category_id
+        and jlt.tracking_category_option_id = tcho.tracking_option_id
+    left join {{ source(cte_name, "tracking_category_option") }} tco
+        on tcho.tracking_option_id = tco.tracking_option_id
+    left join {{ source(cte_name, "tracking_category") }} tc
+        on tc.tracking_category_id = jlt.tracking_category_id
+    left join {{ source("polymer_sp", "coa_metadata_account") }} psp
+        on lower(psp.account_id) = lower(jl.account_id)
+    where tc.name IN (
+           {% for cc in cte_category %}
+        '{{cc}}'
+         
+          {%if not loop.last %} , {% endif %}
+        
+        {% endfor %}
+        )
+    or tc.name is NULL 
+)
+{% if not loop.last %} , {% endif %}
+
+{% endfor %}
+
+{% for cte_name,v in dict_data.items() %}
+
+select {{cte_name}}_combined.*, dd.fiscal_year, dd.fiscal_quarter,dd.yearmonth from {{cte_name}}_combined
+
+left join {{ ref("dim_date") }} dd on {{cte_name}}_combined.journal_date::date = dd.date
+
+{% if not loop.last %}
+union all
+{% endif %}
+{% endfor %}
+{% endmacro %}
